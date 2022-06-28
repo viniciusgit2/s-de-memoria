3c9a54a7dcfb9a2cd9031996349a99917283fb8b
viniciusgit2 <4xv6patelfin@gmail.com>
2022-06-28 09:09:01 -0300

    serio achei mais facil assim do que esse monte de import

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <footer>
+        "aqui é o inicio"
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          texto da pagina
+        </a>
+      </header>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
